620d98303a1077ad1c9699ee1bd819194065e4a1
rumiaktar <ce.rumiakter@gmail.com>
2024-11-24 00:34:04 +0600

    add display books

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -7,11 +7,12 @@ const ListedBooks = () => {
     const books = useLoaderData();
 
     const [readBooks, setReadBooks] = useState([]);
-    
+    const [displayBooks, setDisplayBooks] = useState([]);
 
-    useEffect(() =>{
+
+    useEffect(() => {
         const storedBookIds = getStoredBook();
-        if(books.length > 0){
+        if (books.length > 0) {
             // ====1st rule
             // const readBook = books.filter(book=> storedBookIds.includes(book.id))
             // setReadBooks(readBook)
@@ -19,21 +20,26 @@ const ListedBooks = () => {
 
             // 2nd rule
             const readBook = [];
-            for(const id of storedBookIds){
-                const book = books.find(job => job.id ===id)
-                if(book){
+            for (const id of storedBookIds) {
+                const book = books.find(job => job.id === id)
+                if (book) {
                     readBook.push(book)
                 }
             }
 
             setReadBooks(readBook);
+            setDisplayBooks(readBook);
 
             // console.log(books, storedBookIds, readBook);
         }
-    } ,[])
+    }, [books])
     return (
         <div>
             <h1>This read list:{readBooks.length}</h1>
+            {
+                displayBooks.map(book => <p>{book.bookName}</p>)
+            }
+            
         </div>
     );
 };
